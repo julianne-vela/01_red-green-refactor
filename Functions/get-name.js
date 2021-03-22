c9f70b2c1663e45f31b478fc79aab1a54afdc6fe
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-03-22 14:01:25 -0700

    getName test and function completed

---
+++ Functions/get-name.js
@@ -0,0 +1,5 @@
+const getName = (characterObj) => {
+    return characterObj.name;
+};
+
+module.exports = getName;
--- demo/rgb-to-hex.js
+++ demo/rgb-to-hex.js
@@ -1,11 +1,11 @@
-const decimalToHex = (dec) => dec.toString(16).padStart(2, '0');
+// const decimalToHex = (dec) => dec.toString(16).padStart(2, '0');
 
-const rgbToHex = ({ red, green, blue }) => {
-  const redInHex = decimalToHex(red);
-  const greenInHex = decimalToHex(green);
-  const blueInHex = decimalToHex(blue);
+// const rgbToHex = ({ red, green, blue }) => {
+//   const redInHex = decimalToHex(red);
+//   const greenInHex = decimalToHex(green);
+//   const blueInHex = decimalToHex(blue);
 
-  return `#${redInHex}${greenInHex}${blueInHex}`.toUpperCase();
-};
+//   return `#${redInHex}${greenInHex}${blueInHex}`.toUpperCase();
+// };
 
-module.exports = rgbToHex;
+// module.exports = rgbToHex;
