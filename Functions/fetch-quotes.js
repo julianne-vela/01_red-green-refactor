13a438c70475f0933512009f04941e86559c42c8
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-03-23 09:31:55 -0700

    yay - got the mock test to work properly

--- Functions/fetch-quotes.js
+++ Functions/fetch-quotes.js
@@ -3,9 +3,13 @@ const fetch = require('node-fetch');
 const fetchQuotes = async() => {
     const res = await fetch('http://futuramaapi.herokuapp.com/api/quotes/1');
 
-    const body = await res.json();
+    const [{ character: name, quote: text, image }] = await res.json();
     
-    return body[0];
+    return {
+        name,
+        text,
+        image
+    };
 };
 
 module.exports = fetchQuotes;
--- Tests/fetch-quotes.test.js
+++ Tests/fetch-quotes.test.js
@@ -1,14 +1,23 @@
 const fetchQuotes = require('../Functions/fetch-quotes');
+jest.mock('node-fetch', () => () => 
+    Promise.resolve({
+        json: () => Promise.resolve([{
+            character: 'Professor Farnsworth',
+            quote: 'Now, now, there will be plenty of time to discuss your objections when and if\nyou return.',
+            image: 'https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904014/farnsworth.png'
+        }])
+                
+    }));
 
 describe('fetch quotes function', () => {
-    it('should return a single quote from the API with the name, text, and image associated with it', async () => {
-        const quote = await fetchQuotes();
+    it('should return a single quote with character, quote, and image properties', async() => {
+        const quote = await fetchQuotes();            
 
         expect(quote).toEqual(
-            expect.objectContaining({
-                character: expect.any(String),
-                quote: expect.any(String),
-                image: expect.any(String),
-            }));
+            {
+                name: expect.any(String),
+                text: expect.any(String),
+                image: expect.any(String)
+            });
     });
 });
